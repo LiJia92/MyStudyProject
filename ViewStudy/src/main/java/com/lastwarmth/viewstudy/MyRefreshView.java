508dae1b51b67fb4c644d71ec7a04ee455937f47
lijia <243244898@qq.com>
2015-11-23 17:46:37 +0800

    path初识

--- ViewStudy/src/main/java/com/lastwarmth/viewstudy/MyRefreshView.java
+++ ViewStudy/src/main/java/com/lastwarmth/viewstudy/MyRefreshView.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.util.TypedValue;
@@ -24,27 +25,36 @@ public class MyRefreshView extends View {
     private int angle = 0;
     private ValueAnimator changeAngle;
     private ObjectAnimator animator;
+    private Path mPath;
 
     public MyRefreshView(Context context, AttributeSet attrs) {
         super(context, attrs);
         mContext = context;
         mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaint.setColor(Color.RED);
-        mPaint.setStyle(Paint.Style.STROKE);
+//        mPaint.setStyle(Paint.Style.STROKE);
         mPaint.setStrokeWidth(dip2px(2));
         radius = dip2px(50);
         mOval = new RectF(2, 2, radius * 2, radius * 2);
+        mPath = new Path();
     }
 
 
     @Override
     protected void onDraw(Canvas canvas) {
-        if (changeAngle == null) {
-            canvas.drawArc(mOval, -90, angle, false, mPaint);
-            startAnimation();
-        } else {
-            canvas.drawArc(mOval, -90, angle, false, mPaint);
-        }
+//        if (changeAngle == null) {
+//            canvas.drawArc(mOval, -90, angle, false, mPaint);
+//            startAnimation();
+//        } else {
+//            canvas.drawArc(mOval, -90, angle, false, mPaint);
+//        }
+        mPaint.reset();
+        mPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setColor(Color.GREEN);
+        mPath.moveTo(100, 320);//设置Path的起点
+        mPath.quadTo(200, 500, 300, 300); //设置贝塞尔曲线的控制点坐标和终点坐标
+        canvas.drawPath(mPath, mPaint);//画出贝塞尔曲线
     }
 
     public int dip2px(int dp) {
@@ -61,7 +71,7 @@ public class MyRefreshView extends View {
 //    }
 
     public void startAnimation() {
-        changeAngle = new ValueAnimator().ofInt(0,360);
+        changeAngle = new ValueAnimator().ofInt(0, 360);
         changeAngle.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
