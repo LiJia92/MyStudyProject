77225c02a1243d9b3af8a2846ea6e933131374a0
lijia <243244898@qq.com>
2015-11-24 11:13:07 +0800

    fragment生命周期以及DialogFragment的使用

---
+++ FragmentStudy/src/main/java/com/lastwarmth/fragmentstudy/MyDialogFragment.java
@@ -0,0 +1,37 @@
+package com.lastwarmth.fragmentstudy;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2015/11/24.
+ */
+public class MyDialogFragment extends DialogFragment {
+
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // Get the layout inflater
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.fragment_dialog, null);
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        builder.setView(view)
+                // Add action buttons
+                .setPositiveButton("Sign in",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+
+                            }
+                        }).setNegativeButton("Cancel", null);
+        return builder.create();
+    }
+
+}
