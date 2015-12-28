a2d7a96bd0b18bb5cb53cc4c09ae63603242b803
Jaceli <243244898@qq.com>
2015-12-28 17:13:23 +0800

    MediaPlayer学习，播放在线音乐

---
+++ MediaPlayerStudy/src/main/java/com/example/miali/mediaplayerstudy/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.miali.mediaplayerstudy;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnPause, btnPlayUrl, btnStop;
+    private SeekBar skbProgress;
+    private Player player;
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.setTitle("在线音乐播放---hellogv编写");
+
+        btnPlayUrl = (Button) this.findViewById(R.id.btnPlayUrl);
+        btnPlayUrl.setOnClickListener(new ClickEvent());
+
+        btnPause = (Button) this.findViewById(R.id.btnPause);
+        btnPause.setOnClickListener(new ClickEvent());
+
+        btnStop = (Button) this.findViewById(R.id.btnStop);
+        btnStop.setOnClickListener(new ClickEvent());
+
+        skbProgress = (SeekBar) this.findViewById(R.id.skbProgress);
+        skbProgress.setOnSeekBarChangeListener(new SeekBarChangeEvent());
+        player = new Player(skbProgress);
+
+    }
+
+    class ClickEvent implements View.OnClickListener {
+
+        @Override
+        public void onClick(View arg0) {
+            if (arg0 == btnPause) {
+                player.pause();
+            } else if (arg0 == btnPlayUrl) {
+                //在百度MP3里随便搜索到的,大家可以试试别的链接
+                String url = "http://10.0.0.36:8080/MiaMusic/blandness.mp3";
+                player.playUrl(0);
+            } else if (arg0 == btnStop) {
+                player.stop();
+            }
+        }
+    }
+
+    class SeekBarChangeEvent implements SeekBar.OnSeekBarChangeListener {
+        int progress;
+
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress,
+                                      boolean fromUser) {
+            // 原本是(progress/seekBar.getMax())*player.mediaPlayer.getDuration()
+            this.progress = progress * player.mediaPlayer.getDuration()
+                    / seekBar.getMax();
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+            // seekTo()的参数是相对与影片时间的数字，而不是与seekBar.getMax()相对的数字
+            player.mediaPlayer.seekTo(progress);
+        }
+    }
+
+}
---
+++ MediaPlayerStudy/src/main/java/com/example/miali/mediaplayerstudy/Player.java
@@ -0,0 +1,139 @@
+package com.example.miali.mediaplayerstudy;
+
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.widget.SeekBar;
+
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by Lijia on 2015-12-28.
+ */
+public class Player implements MediaPlayer.OnBufferingUpdateListener,
+        MediaPlayer.OnCompletionListener, MediaPlayer.OnPreparedListener {
+    public MediaPlayer mediaPlayer;
+    private SeekBar skbProgress;
+    private Timer mTimer = new Timer();
+
+
+    private String[] urls = {"http://10.0.0.36:8080/MiaMusic/Aimer%20-%20LAST%20STARDUST.mp3",
+            "http://10.0.0.36:8080/MiaMusic/Aimer%20-%20Brave%20Shine.mp3",
+            "http://10.0.0.36:8080/MiaMusic/blandness.mp3"};
+    private int currentMusic = 0;
+
+    public Player(SeekBar skbProgress) {
+        this.skbProgress = skbProgress;
+
+        try {
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            mediaPlayer.setOnBufferingUpdateListener(this);
+            mediaPlayer.setOnCompletionListener(this);
+            mediaPlayer.setOnPreparedListener(this);
+        } catch (Exception e) {
+            Log.e("mediaPlayer", "error", e);
+        }
+
+        mTimer.schedule(mTimerTask, 0, 1000);
+    }
+
+    /*******************************************************
+     * 通过定时器和Handler来更新进度条
+     ******************************************************/
+    TimerTask mTimerTask = new TimerTask() {
+        @Override
+        public void run() {
+            if (mediaPlayer == null)
+                return;
+            if (mediaPlayer.isPlaying() && skbProgress.isPressed() == false) {
+                handleProgress.sendEmptyMessage(0);
+            }
+        }
+    };
+
+    Handler handleProgress = new Handler() {
+        public void handleMessage(Message msg) {
+
+            int position = mediaPlayer.getCurrentPosition();
+            int duration = mediaPlayer.getDuration();
+
+            if (duration > 0) {
+                long pos = skbProgress.getMax() * position / duration;
+                skbProgress.setProgress((int) pos);
+            }
+        }
+
+        ;
+    };
+    //*****************************************************
+
+    public void play() {
+        mediaPlayer.start();
+    }
+
+    public void playUrl(int index) {
+        try {
+            currentMusic = index;
+            mediaPlayer.reset();
+            mediaPlayer.setDataSource(urls[index]);
+            mediaPlayer.prepare();//prepare之后自动播放
+            //mediaPlayer.start();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void pause() {
+        mediaPlayer.pause();
+    }
+
+    public void stop() {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+
+    @Override
+    /**
+     * 通过onPrepared播放
+     */
+    public void onPrepared(MediaPlayer arg0) {
+        arg0.start();
+        Log.e("mediaPlayer", "onPrepared");
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer arg0) {
+        Log.e("mediaPlayer", "onCompletion");
+        if (currentMusic < urls.length - 1) {
+            currentMusic++;
+            try {
+                mediaPlayer.reset();
+                mediaPlayer.setDataSource(urls[currentMusic]);
+                mediaPlayer.prepare();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onBufferingUpdate(MediaPlayer arg0, int bufferingProgress) {
+        skbProgress.setSecondaryProgress(bufferingProgress);
+        int currentProgress = skbProgress.getMax() * mediaPlayer.getCurrentPosition() / mediaPlayer.getDuration();
+        Log.e(currentProgress + "% play", bufferingProgress + "% buffer");
+    }
+
+}
