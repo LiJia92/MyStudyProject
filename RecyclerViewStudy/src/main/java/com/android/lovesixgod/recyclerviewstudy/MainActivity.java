c9f68bb9dc1569162bc28fe61986e06240127544
Jaceli <243244898@qq.com>
2016-03-09 17:18:16 +0800

    RecyclerView使用StaggeredGridLayoutManager，数据增删的动画

--- RecyclerViewStudy/src/main/java/com/android/lovesixgod/recyclerviewstudy/MainActivity.java
+++ RecyclerViewStudy/src/main/java/com/android/lovesixgod/recyclerviewstudy/MainActivity.java
@@ -1,17 +1,18 @@
 package com.android.lovesixgod.recyclerviewstudy;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +20,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private List<String> data;
+    private MyRecyclerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,16 +33,19 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                adapter.addData(1);
             }
         });
 
         initData();
+        adapter = new MyRecyclerAdapter(this, data);
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 //        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-        recyclerView.setLayoutManager(new GridLayoutManager(this, 4));
-        recyclerView.setAdapter(new MyRecyclerAdapter());
+//        recyclerView.setLayoutManager(new GridLayoutManager(this, 4));
+        recyclerView.setLayoutManager(new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL));
+        recyclerView.setAdapter(adapter);
 //        recyclerView.addItemDecoration(new ListItemDecoration(this, LinearLayoutManager.VERTICAL));
 //        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL_LIST));
         recyclerView.addItemDecoration(new DividerGridItemDecoration(this));
@@ -55,6 +60,20 @@ public class MainActivity extends AppCompatActivity {
 
     private class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.MyViewHolder> {
 
+        private List<Integer> heights;
+        private Context context;
+        private List<String> data;
+
+        public MyRecyclerAdapter(Context context, List<String> data) {
+            this.context = context;
+            this.data = data;
+
+            heights = new ArrayList<>();
+            for (int i = 0; i < data.size(); i++) {
+                heights.add((int) (150 + Math.random() * 200));
+            }
+        }
+
         @Override
         public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             View view = getLayoutInflater().inflate(R.layout.item, parent, false);
@@ -63,6 +82,9 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onBindViewHolder(MyViewHolder holder, int position) {
+            ViewGroup.LayoutParams params = holder.itemView.getLayoutParams();
+            params.height = heights.get(position);
+            holder.itemView.setLayoutParams(params);
             holder.numText.setText(data.get(position));
         }
 
@@ -78,7 +100,32 @@ public class MainActivity extends AppCompatActivity {
             public MyViewHolder(View itemView) {
                 super(itemView);
                 numText = (TextView) itemView.findViewById(R.id.id_num);
+
+                itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(context, "onClick", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                    @Override
+                    public boolean onLongClick(View v) {
+                        Toast.makeText(context, "onLongClick", Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
             }
+        }
+
+        public void addData(int position) {
+            data.add(position, "Insert One");
+            notifyItemInserted(position);
+        }
+
+        public void removeData(int position) {
+            data.remove(position);
+            notifyItemRemoved(position);
         }
     }
 
@@ -98,6 +145,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            adapter.removeData(1);
             return true;
         }
 
