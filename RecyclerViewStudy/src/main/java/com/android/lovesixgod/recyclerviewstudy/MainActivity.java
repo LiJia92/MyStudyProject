cdea75eb12d337a2248b70c6b32c0f7158f977ac
Jaceli <243244898@qq.com>
2016-03-09 16:30:27 +0800

    RecyclerView使用GridLayoutManager

--- RecyclerViewStudy/src/main/java/com/android/lovesixgod/recyclerviewstudy/MainActivity.java
+++ RecyclerViewStudy/src/main/java/com/android/lovesixgod/recyclerviewstudy/MainActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -38,10 +38,12 @@ public class MainActivity extends AppCompatActivity {
 
         initData();
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 4));
         recyclerView.setAdapter(new MyRecyclerAdapter());
 //        recyclerView.addItemDecoration(new ListItemDecoration(this, LinearLayoutManager.VERTICAL));
-        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL_LIST));
+//        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL_LIST));
+        recyclerView.addItemDecoration(new DividerGridItemDecoration(this));
     }
 
     protected void initData() {
